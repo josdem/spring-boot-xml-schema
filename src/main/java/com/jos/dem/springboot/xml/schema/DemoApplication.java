2ee6ef6535f8c0d1150bd06d4a481fbe5d14e07e
josdem <joseluis.delacruz@gmail.com>
2019-08-25 17:23:35 -0400

    Printing person values with apache commons lang3

--- src/main/java/com/jos/dem/springboot/xml/schema/DemoApplication.java
+++ src/main/java/com/jos/dem/springboot/xml/schema/DemoApplication.java
@@ -1,21 +1,30 @@
-package com.example.springbootxmlschema;
+package com.jos.dem.springboot.xml.schema;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import com.jos.dem.springboot.xml.schema.service.PersonService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+  private Logger log = LoggerFactory.getLogger(this.getClass());
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 
   @Bean
-  CommandLineRunner start(){
+  CommandLineRunner start(PersonService personService){
     return args -> {
-      System.out.println("Hello World!");
+      log.info("Person: {}", ToStringBuilder.reflectionToString(personService.create()));
     };
   }
 
