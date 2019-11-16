7d97004aaf7c80de29951ec5bf72e2f62caa860c
josdem <joseluis.delacruz@gmail.com>
2019-11-16 15:54:45 -0500

    Adding test case

--- src/main/java/com/jos/dem/springboot/xml/schema/service/impl/PersonServiceImpl.java
+++ src/main/java/com/jos/dem/springboot/xml/schema/service/impl/PersonServiceImpl.java
@@ -11,7 +11,7 @@ public class PersonServiceImpl implements PersonService {
     Person person =  new Person();
     person.setFirstName("Jose");
     person.setLastName("Morales");
-    person.setAddress("30 Frank Llord Dr, Ann Arbor, Michigan");
+    person.setAddress("30 Frank Lloyd, Ann Arbor MI 48105");
     return person;
   }
 
---
+++ src/test/java/com/jos/dem/springboot/xml/schema/DemoApplicationTests.java
@@ -0,0 +1,31 @@
+package com.jos.dem.springboot.xml.schema;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.jos.dem.entities.Person;
+import com.jos.dem.springboot.xml.schema.service.PersonService;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Autowired
+	private PersonService personService;
+
+	@Test
+	void shouldCreatePerson() {
+    Person person = personService.create();
+
+    assertAll("person",
+        () -> assertEquals("Jose", person.getFirstName(), "should get first name"),
+        () -> assertEquals("Morales", person.getLastName(), "should get last name"),
+        () -> assertEquals("30 Frank Lloyd, Ann Arbor MI 48105", person.getAddress(), "should get address")
+        );
+  }
+
+}
